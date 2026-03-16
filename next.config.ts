6822c3e9f6f03ceccf341c9ff485c08a4de55b06
j-riedemann <riedemann86@hotmail.com>
2026-03-16 20:13:49 +0000

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
-export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
